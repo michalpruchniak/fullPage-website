aef3415a9e2baa0d5e91749370ec80d3746307a6
Michal Pruchniak <mpruchniak@gmail.com>
2017-02-17 14:07:12 +0100

    przeniesienie kodu js do pliku main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-/*
-$(function(){
-  $( "nav li" ).mouseover(function() {
-    $("#first").addClass("hover-background");
-    $("#first").removeClass("mouseout-background");
-  }).mouseout(function(){
-    $("#first").removeClass("hover-background");
-    $("#first").addClass("mouseout-background");
-  })
+$('#fullpage').fullpage({
+    anchors: ['start', 'about', 'portfolio', 'contact'],
+    menu: 'menu',
+    navigation: true,
+    sectionsColor: [undefined, '#4BBFC3', '#7BAABE', '#E91E63'],
+    afterLoad: function(undefined, index){
+      $('#menu li').removeClass("active");
+      $('#menu li').eq(index-1).addClass("active");
+    }
 });
-*/
