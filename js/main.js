e9c32df159963098aaac1ee5c8f55774891d3260
Michal Pruchniak <mpruchniak@gmail.com>
2017-02-17 13:57:24 +0100

    Rozpoczęcie pracy

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*
+$(function(){
+  $( "nav li" ).mouseover(function() {
+    $("#first").addClass("hover-background");
+    $("#first").removeClass("mouseout-background");
+  }).mouseout(function(){
+    $("#first").removeClass("hover-background");
+    $("#first").addClass("mouseout-background");
+  })
+});
+*/
