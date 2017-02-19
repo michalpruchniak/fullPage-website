8198f7c22e8133684356c98ebd23f1ffbcc12183
Michal Pruchniak <mpruchniak@gmail.com>
2017-02-19 12:07:12 +0100

    portfolio

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $('#fullpage').fullpage({
-    anchors: ['start', 'about', 'portfolio', 'contact'],
+    anchors: ['start', 'o-mnie', 'moje-portfolio', 'kontakt'],
     menu: 'menu',
     navigation: true,
-    sectionsColor: [undefined, '#4BBFC3', '#7BAABE', '#E91E63'],
+    sectionsColor: [undefined, '#4BBFC3', '#BA68C8', '#E91E63'],
     afterLoad: function(undefined, index){
       $('#menu li').removeClass("active");
       $('#menu li').eq(index-1).addClass("active");
