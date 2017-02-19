78c6dcdd5a0a03e2bafe680a0b7c64c2b5f39bbc
Michal Pruchniak <mpruchniak@gmail.com>
2017-02-19 15:21:06 +0100

    kontakt

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $('#fullpage').fullpage({
     anchors: ['start', 'o-mnie', 'moje-portfolio', 'kontakt'],
     menu: 'menu',
     navigation: true,
-    sectionsColor: [undefined, '#4BBFC3', '#BA68C8', '#E91E63'],
+    sectionsColor: [undefined, '#4BBFC3', '#BA68C8', '#80CBC4'],
     afterLoad: function(undefined, index){
       $('#menu li').removeClass("active");
       $('#menu li').eq(index-1).addClass("active");
